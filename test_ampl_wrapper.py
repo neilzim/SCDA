b26e00298d29744c5c9492fe101f4f47719c3eb2
Neil Zimmerman <neiltzim@gmail.com>
2016-02-17 23:01:48 -0500

    more irisao parameters set for testing

--- test_ampl_wrapper.py
+++ test_ampl_wrapper.py
@@ -18,22 +18,22 @@ if __name__ == "__main__":
     ampl_src_dir = os.path.join(os.getcwd(), "test_scda_aplc_new") # nominal destination for new AMPL programs
     fileorg = {'ampl src dir': ampl_src_dir}
 
-    # Design parameters, encoded in a nested dictionary
+    # IrisAO design parameters
     pupil_params = {'N': 200,'tel diam': 12.}
-    fpm_params = {'rad': 4.}
+    fpm_params = {'rad': 9.898/2, 'M':50}
     ls_params = {'id': 10, 'od': 0.9}
-    image_params = {'c': 10.1}
+    image_params = {'c': 10., 'iwa':3.5, 'owa':10., 'owa2':25.}
     design_params = {'Pupil': pupil_params, 'FPM': fpm_params, 'LS': ls_params, 'Image': image_params}
     # Options for constraints and optimizer
     solver_params = {'method': 'bar', 'presolve': False}
     
-    test_coron = scda.HalfplaneAPLC(fileorg=fileorg, design=design_params, solver=solver_params)
+    irisao_coron = scda.HalfplaneAPLC(fileorg=fileorg, design=design_params, solver=solver_params)
 
-    print("file organization: {0}".format(test_coron.fileorg))
-    print("solver parameters: {0}".format(test_coron.solver))
-    print("design parameters: {0}".format(test_coron.design))
+    irisao_coron.logger.info("file organization: {0}".format(irisao_coron.fileorg))
+    irisao_coron.logger.info("solver parameters: {0}".format(irisao_coron.solver))
+    irisao_coron.logger.info("design parameters: {0}".format(irisao_coron.design))
 
     print
 #    print("AMPL source file name: {0}".format(test_coron.ampl_src_fname))
 
-    test_coron.write_ampl()
+    irisao_coron.write_ampl()
