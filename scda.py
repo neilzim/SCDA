fdaa66fc5376d8220e0e411b9af23c0b0861fd59
Neil Zimmerman <neiltzim@gmail.com>
2016-02-17 23:00:15 -0500

    progress in AMPL source code generation, and file name structure

--- scda.py
+++ scda.py
@@ -12,22 +12,25 @@ import datetime
 import textwrap
 import numpy as np
 import pdb
+import getpass
 
 def configure_log(log_fname=None):
     logger = logging.getLogger("scda.logger")
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler(sys.stdout) # console dump
-    ch.setLevel(logging.INFO)
-    logger.addHandler(ch)
-    if log_fname is not None: # optional file log in addition to the console dump
-        fh = logging.FileHandler(log_fname, mode="w")
-        fh.setLevel(logging.DEBUG)
-        logger.addHandler(fh)
+    if not len(logger.handlers):
+        logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler(sys.stdout) # console dump
+        ch.setLevel(logging.INFO)
+        logger.addHandler(ch)
+        if log_fname is not None: # optional file log in addition to the console dump
+            fh = logging.FileHandler(log_fname, mode="w")
+            fh.setLevel(logging.DEBUG)
+            logger.addHandler(fh)
 
 class LyotCoronagraph(object): # Lyot coronagraph base class
-    _key_fields = dict([('fileorg', ['ampl src dir', 'tel ap dir', 'FPM dir', 'LS dir', 'sol dir', 'eval dir']),\
+    _key_fields = dict([('fileorg', ['ampl src dir', 'TelAp dir', 'FPM dir', 'LS dir', 'sol dir', 'eval dir']),\
                         ('solver', ['constr', 'method', 'presolve'])])
-    _solver_menu = dict([('constr',['lin', 'quad']), ('method', ['bar', 'barhom', 'dualsimp']), ('presolve',[True, False])])
+    _solver_menu = dict([('constr',['lin', 'quad']), ('method', ['bar', 'barhom', 'dualsimp']), \
+                         ('presolve',[True, False]), ('Nthreads', range(1,33))])
     _aperture_menu = dict([('pm', ['hex1', 'hex2', 'hex3', 'key24', 'pie12', 'pie8', 'irisao']),\
                            ('ss', ['y60','y60off','x','cross','t','y90']),\
                            ('sst', ['025','100']),\
@@ -50,7 +53,7 @@ class LyotCoronagraph(object): # Lyot coronagraph base class
                 if dirkey in self._key_fields['fileorg']:
                     self.fileorg[dirkey] = location
                     if location is not None and not os.path.exists(location):
-                        self.logger.warning("Warning: The specified location for \"{0}\", \"{1}\" does not yet exist".format(dirkey, location))
+                        self.logger.warning("Warning: The specified location for \"{0}\", \"{1}\" does not exist".format(dirkey, location))
                 else:
                     self.logger.warning("Warning: Unrecognized field {0} in fileorg argument".format(dirkey))
         # Handle missing values
@@ -78,13 +81,27 @@ class LyotCoronagraph(object): # Lyot coronagraph base class
         if 'constr' not in self.solver or self.solver['constr'] is None: self.solver['constr'] = 'lin'
         if 'method' not in self.solver or self.solver['method'] is None: self.solver['method'] = 'bar'
         if 'presolve' not in self.solver or self.solver['presolve'] is None: self.solver['presolve'] = True
+        if 'Nthreads' not in self.solver or self.solver['Nthreads'] is None: self.solver['Nthreads'] = None
+
+        if 'TelAp_fname' in kwargs:
+            self.telap_fname = os.path.join(self.fileorg['TelAp dir'], kwargs['TelAp_fname'])
+            if not os.path.exists(self.telap_fname):
+                self.logger.warning("Warning: The specified telescope aperture file \"{0}\" does not exist".format(self.telap_fname))
+        if 'FPM_fname' in kwargs:
+            self.fpm_fname = os.path.join(self.fileorg['FPM dir'], kwargs['FPM_fname'])
+            if not os.path.exists(self.fpm_fname):
+                self.logger.warning("Warning: The specified focal plane mask file \"{0}\" does not exist".format(self.fpm_fname))
+        if 'LS_fname' in kwargs:
+            self.ls_fname = os.path.join(self.fileorg['LS dir'], kwargs['LS_fname'])
+            if not os.path.exists(self.ls_fname):
+                self.logger.warning("Warning: The specified Lyot stop file \"{0}\" does not exist".format(self.ls_fname))
 
 class NdiayeAPLC(LyotCoronagraph): # Image-constrained APLC following N'Diaye et al. (2015, 2016)
     _design_fields = { 'Pupil': {'N':(int, 1000), 'pm':(str, 'hex1'), 'so':(bool, True), 'ss':(str, 'x'), 'sst':(str, '100'), 'tel diam':(float, 12.)}, \
                        'FPM':   {'M':(int, 50), 'rad':(float, 4.)}, \
-                       'LS':    {'id':(float, 0.2), 'od':(float, 0.9), 'ovszfac':(float, None), 'altol':(float, None)}, \
-                       'Image': {'c':(float, 10.), 'c1':(float, None), 'c2':(float, None), 'iwa':(float, 4.), \
-                                 'owa':(float, 10.), 'owa2':(float, None), 'fpres':(int,2), 'bw':(float, 0.1), 'Nlam':(int, 3)} }
+                       'LS':    {'id':(int, 20), 'od':(int, 90), 'ovsz':(int, 0), 'altol':(int, None)}, \
+                       'Image': {'c':(float, 10.), 'ci':(float, None), 'co':(float, None), 'iwa':(float, 4.), \
+                                 'owa':(float, 10.), 'oca':(float, 10.), 'fpres':(int,2), 'bw':(float, 0.1), 'Nlam':(int, 3)} }
     _eval_fields =   { 'Pupil': _design_fields['Pupil'], 'FPM': _design_fields['FPM'], \
                        'LS': _design_fields['LS'], 'Image': _design_fields['Image'], \
                        'Target': {}, 'Aber': {}, 'WFSC': {} }
@@ -119,45 +136,57 @@ class NdiayeAPLC(LyotCoronagraph): # Image-constrained APLC following N'Diaye et
                 if param not in self.design[keycat] or (self.design[keycat][param] is None and \
                                                         self._design_fields[keycat][param][1] is not None):
                     self.design[keycat][param] = self._design_fields[keycat][param][1]
+        # Finally, set a private attribute for the number of image plane samples between the center and the OCA
+        self.design['Image']['_Nimg'] = int( np.ceil( self.design['Image']['fpres']*self.design['Image']['oca']/(1. - self.design['Image']['bw']/2) ) )
      
-        self.amplname_coron = "APLC" 
+        self.amplname_coron = "APLC_full" 
         if self.design['Pupil']['so'] == True:
-            self.amplname_pupil = "{}{}t{}so1D{:04}".format(self.design['Pupil']['pm'], self.design['Pupil']['ss'], self.design['Pupil']['sst'], self.design['Pupil']['N'])
+            self.amplname_pupil = "{0:s}{1:s}t{2:s}so1_N{3:04d}".format(self.design['Pupil']['pm'], self.design['Pupil']['ss'], self.design['Pupil']['sst'], self.design['Pupil']['N'])
         else:                                                                                                                                      
-            self.amplname_pupil = "{}{}t{}so0D{:04}".format(self.design['Pupil']['pm'], self.design['Pupil']['ss'], self.design['Pupil']['sst'], self.design['Pupil']['N'])
+            self.amplname_pupil = "{0:s}{1:s}t{2:s}so0_N{3:04d}".format(self.design['Pupil']['pm'], self.design['Pupil']['ss'], self.design['Pupil']['sst'], self.design['Pupil']['N'])
 
         self.amplname_fpm = "FPM{:02}M{:03}".format(int(round(100*self.design['FPM']['rad'])), self.design['FPM']['M'])
-        if self.design['LS']['altol'] is None and self.design['LS']['ovszfac'] is None:
-            self.amplname_ls = "LS{:02}D{:02}ovsz{:02}tol{:02}".format(int(round(100*self.design['LS']['id'])), \
-                               int(round(100*self.design['LS']['od'])), 0, 0)
-        elif self.design['LS']['altol'] is None and self.design['LS']['ovszfac'] is not None:
-            self.amplname_ls = "LS{:02}D{:02}ovsz{:02}tol{:02}".format(int(round(100*self.design['LS']['id'])), \
-                               int(round(100*self.design['LS']['od'])), int(round(100*self.design['LS']['ovszfac'])), 0)
-        elif self.design['LS']['altol'] is not None and self.design['LS']['ovszfac'] is None:
-            self.amplname_ls = "LS{:02}D{:02}ovsz{:02}tol{:02}".format(int(round(100*self.design['LS']['id'])), \
-                               int(round(100*self.design['LS']['od'])), 0, int(round(100*self.design['LS']['altol'])))
+        if self.design['LS']['altol'] is None and self.design['LS']['ovsz'] is None:
+            self.amplname_ls = "LS{:02}D{:02}ovsz{:02}tol{:02}".format(self.design['LS']['id'], \
+                               self.design['LS']['od'], 0, 0)
+        elif self.design['LS']['altol'] is None and self.design['LS']['ovsz'] is not None:
+            self.amplname_ls = "LS{:02}D{:02}ovsz{:02}tol{:02}".format(self.design['LS']['id'], \
+                               self.design['LS']['od'], self.design['LS']['ovsz'], 0)
+        elif self.design['LS']['altol'] is not None and self.design['LS']['ovsz'] is None:
+            self.amplname_ls = "LS{:02}D{:02}ovsz{:02}tol{:02}".format(self.design['LS']['id'], \
+                               self.design['LS']['od'], 0, self.design['LS']['altol'])
         else:
-            self.amplname_ls = "LS{:02}D{:02}ovsz{:02}tol{:02}".format(int(round(100*self.design['LS']['id'])), \
-                               int(round(100*self.design['LS']['od'])), int(round(100*self.design['LS']['ovszfac'])), \
-                               int(round(100*self.design['LS']['altol'])))
+            self.amplname_ls = "LS{:02}D{:02}ovsz{:02}tol{:02}".format(self.design['LS']['id'], \
+                               self.design['LS']['od'], self.design['LS']['ovsz'], self.design['LS']['altol'])
 
-        self.amplname_image = "Img{:03}C{:02}WA{:03}BW{:02}Nlam{:02}fpres{:1}".format(int(round(10*self.design['Image']['c'])), \
+        self.amplname_image = "Img{:03}C_{:02}WA{:03}CA{:03}_BW{:02}Nlam{:02}fpres{:1}".format(int(round(10*self.design['Image']['c'])), \
                                int(round(10*self.design['Image']['iwa'])), int(round(10*self.design['Image']['owa'])), \
+                               int(round(10*self.design['Image']['oca'])), \
                                int(round(100*self.design['Image']['bw'])), self.design['Image']['Nlam'], self.design['Image']['fpres'])
-        if self.design['Image']['c1'] is not None:
-            self.amplname_image += "C1{:03}".format(self.design['Image']['c1'])
-        if self.design['Image']['c2'] is not None:
-            self.amplname_image += "C2{:03}".format(self.design['Image']['c2'])
-        if self.design['Image']['owa2'] is not None:
-            self.amplname_image += "OWA2{:03}".format(self.design['Image']['owa2'])
+        if self.design['Image']['ci'] is not None:
+            self.amplname_image += "Cin{:03}".format(self.design['Image']['ci'])
+        if self.design['Image']['co'] is not None:
+            self.amplname_image += "Cout{:03}".format(self.design['Image']['co'])
 
         if self.solver['presolve']:
             self.amplname_solver = "{}{}pre1".format(self.solver['constr'], self.solver['method'])
         else:
             self.amplname_solver = "{}{}pre0".format(self.solver['constr'], self.solver['method'])
+        if self.solver['Nthreads'] is not None:
+            self.amplname_solver += "threads{:02d}".format(self.solver['Nthreads'])
 
         self.ampl_src_fname = self.amplname_coron + "_" + self.amplname_pupil + "_" + self.amplname_fpm + "_" + \
                               self.amplname_ls + "_" + self.amplname_image + "_" + self.amplname_solver + ".mod" 
+
+        if not hasattr(self, 'telap_fname'):
+            self.telap_fname = os.path.join( self.fileorg['TelAp dir'], ("TelAp_full_" + self.amplname_pupil + ".dat") )
+        if not hasattr(self, 'fpm_fname'):
+            self.fpm_fname = os.path.join( self.fileorg['FPM dir'], "FPM_occspot_M{:03}.dat".format(self.design['FPM']['M']) )
+        if not hasattr(self, 'ls_fname'):
+            self.ls_fname = os.path.join( self.fileorg['LS dir'], ("LS_full_" + self.amplname_pupil + \
+                                          "_{0:02d}D{1:02d}ovsz{2:02d}.dat".format(self.design['LS']['id'], \
+                                          self.design['LS']['od'], self.design['LS']['ovsz'])) ) 
+                                                              
 
     def write_ampl(self):
         self.logger.info("Writing the AMPL program")
@@ -171,9 +200,17 @@ class NdiayeAPLC(LyotCoronagraph): # Image-constrained APLC following N'Diaye et
 class HalfplaneAPLC(NdiayeAPLC): # N'Diaye APLC subclass for the half-plane symmetry case
     def __init__(self, **kwargs):
         super(HalfplaneAPLC, self).__init__(**kwargs)
-        self.amplname_coron = "hpAPLC" 
+        self.amplname_coron = "APLC_half" 
         self.ampl_src_fname = self.amplname_coron + "_" + self.amplname_pupil + "_" + self.amplname_fpm + "_" + \
                               self.amplname_ls + "_" + self.amplname_image + "_" + self.amplname_solver + ".mod" 
+        if not hasattr(self, 'telap_fname') or os.path.split(self.telap_fname)[1].startswith("TelAp_full"):
+            self.telap_fname = os.path.join(os.path.split(self.telap_fname)[0], \
+                                            os.path.split(self.telap_fname)[1].replace('TelAp_full', 'TelAp_half'))
+        if not hasattr(self, 'fpm_fname'):
+            self.fpm_fname = os.path.join( self.fileorg['FPM dir'], "FPM_occspot_M{:03}.dat".format(self.design['FPM']['M']) )
+        if not hasattr(self, 'ls_fname') or os.path.split(self.ls_fname)[1].startswith("LS_full"):
+            self.ls_fname = os.path.join(os.path.split(self.ls_fname)[0], \
+                                         os.path.split(self.ls_fname)[1].replace('LS_full', 'LS_half'))
 
     def write_ampl(self): 
         self.logger.info("Writing the AMPL program for this APLC with halfplane symmetry")
@@ -185,13 +222,80 @@ class HalfplaneAPLC(NdiayeAPLC): # N'Diaye APLC subclass for the half-plane symm
         mod_fobj = open(self.abs_ampl_src_fname, "w")
 
         header = """\
-        # AMPL program to optimize an APLC with half-plane symmetry
-        # Created with {0} at {1}
+        # AMPL program to optimize a half-plane symmetric APLC
+        # Created by {0} with {1} at {2}
         load amplgsl.dll;
+
+
+        #---------------------
+
         param pi:= 4*atan(1);
-        """.format(os.path.basename(__file__), datetime.datetime.now())
+
+        #---------------------
+        param c := {3:.2f};
+
+        #---------------------
+        param Rmask := {4:0.3f};
+        param rho0 := {5:0.2f};
+        param rho1 := {6:0.2f};
+        
+        #---------------------
+        param N := {7};				# discretization parameter (pupil)
+        param M := {8};				# discretization parameter (mask)
+        
+        param Nimg := {9};			# discretization parameter (image)
+        param Fmax := {10:0.2f};    # NTZ: If we paramaterize our image plane resolution by fpres = sampling rate at 
+                                    # the shortest wavelength, then Nimg should be an integer function of fpres, oca,
+        #---------------------      # and bw. This integer is not specified directly by the user, but computed "privately"
+        param bw := {11:0.2f};      # by the APLC class constructor.
+        param lam0 := 1.;
+        param dl := bw*lam0;
+        param Nlam := {12};
+        
+        #---------------------
+        param obs := 20;             # NTZ: We could eliminate this section of parameter definitions, 
+        param spiders :=01;          #      since we determine the file names of the aperture and Lyot stop
+        param lsobs := 20;           #      outside the program, as well as the file name of the apodizer solution.
+        param lsspiders :=02;
+        param gap :=01;
+        param lsgap :=00;
+        
+        param OD :=0.98;
+        
+        #---------------------
+        param Normterm := 1.00; 	# 0.380
+        
+        #---------------------
+        param CoeffOverSizePup :=0.824*OD;
+        """.format(getpass.getuser(), os.path.basename(__file__), datetime.datetime.now(), \
+                   self.design['Image']['c'], self.design['FPM']['rad'], self.design['Image']['iwa'], self.design['Image']['owa'], \
+                   self.design['Pupil']['N'], self.design['FPM']['M'], self.design['Image']['_Nimg'], \
+                   self.design['Image']['oca'], self.design['Image']['bw'], self.design['Image']['Nlam'])
+        
+        load_masks = """\
+        #---------------------
+        # Loading Pupil
+        param PupilFile {{1..2*N,1..N}};
+        
+        read {{i in 1..2*N,j in 1..N}} PupilFile[i,j] < {0:s};
+        close {1:s};
+        
+        # Loading FPM
+        param MaskFile {{1..2*M,1..M}};
+        
+        read {{i in 1..2*M,j in 1..M}} MaskFile[i,j] < {2:s}; 
+        close {3:s};
+        
+        # Loading Lyot stop
+        param LyotFile {{1..2*N,1..N}};
+        
+        read {{i in 1..2*N,j in 1..N}} LyotFile[i,j] < {4:s};
+        close {5:s};
+        """.format(self.telap_fname, self.telap_fname, self.fpm_fname, self.fpm_fname, self.ls_fname, self.ls_fname)
+
 
         mod_fobj.write( textwrap.dedent(header) )
+        mod_fobj.write( textwrap.dedent(load_masks) )
 
         mod_fobj.close()
         self.logger.info("Wrote %s"%self.abs_ampl_src_fname)
